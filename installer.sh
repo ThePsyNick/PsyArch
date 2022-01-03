097545230266cbc11a8ac98bba1d9c77602398d5
ThePsyNick <92543628+ThePsyNick@users.noreply.github.com>
2022-01-03 01:15:00 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,19 +8,20 @@ mount /dev/sda3 /mnt
 swapon /dev/sda2
 pacstrap /mnt base linux linux-firmware networkmanager vim sudo grub efibootmgr
 genfstab -U /mnt >> /mnt/etc/fstab
-ln -sf /mnt/usr/share/zoneinfo/Europe/Rome >> /mnt/etc/localtime
-hwclock --systohc
-echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
-echo "LANG=en_US.UTF-8" >> /mnt/etc/locale.conf
-echo "KEYMAP=it" >> /mnt/etc/vconsole.conf
+arch-chroot /mnt
+expect ln -sf /usr/share/zoneinfo/Europe/Rome >> /etc/localtime
+expect hwclock --systohc
+expect echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+expect echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+expect echo "KEYMAP=it" >> /etc/vconsole.conf
 echo "Set an Hostname for the installation: "
 read hostprompt
-echo $hostprompt >> /mnt/etc/hostname
+echo $hostprompt >> /etc/hostname
 echo "Set Password for Root User:"
-passwd
-cd /mnt/
-mkdir ./boot/efi
-mount /dev/sda1 ./boot/efi
-grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=./boot/efi
-grub-mkconfig -o ./boot/grub/grub.cfg
+expect passwd
+expect mkdir /boot/efi
+expect mount /dev/sda1 /boot/efi
+expect grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+expect grub-mkconfig -o /boot/grub/grub.cfg
+expect exit
 echo "Installation Finished!"
